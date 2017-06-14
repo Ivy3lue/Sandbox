118da73ed2650afb09dd751c6ab133de1ca1f139
Ivy <gericaleksandra@gmail.com>
2017-06-14 20:40:11 +0200

    Initial commit

---
+++ src/com/ivy/Main.java
@@ -0,0 +1,14 @@
+package com.ivy;
+
+public class Main {
+    /**
+     * This is Java's main method
+     * <br>
+     * It always looks like this and each program must have one!
+     *
+     * @param args the arguments passed to the program
+     */
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
