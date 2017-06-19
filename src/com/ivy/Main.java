87e6d70ac1d7aff662915b79b5bb17e8e8a43850
Ivy <gericaleksandra@gmail.com>
2017-06-19 23:04:35 +0200

    Print a "K" matrix
    This example demonstrates using two double for loops

--- src/com/ivy/Main.java
+++ src/com/ivy/Main.java
@@ -18,7 +18,13 @@ public class Main {
         int order = scanner.nextInt();
 
         for (int row = 0; row < order; row++) {
-            for (int column = 0; column < order; column++) {
+            for (int column = 0; column < order - row; column++) {
+                System.out.print("O ");
+            }
+            System.out.println();
+        }
+        for (int row = 1; row < order; row++) {
+            for (int column = 0; column <= row; column++) {
                 System.out.print("O ");
             }
             System.out.println();
