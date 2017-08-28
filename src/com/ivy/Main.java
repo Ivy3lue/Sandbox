35608f4505ed197b65748d114191815f0e771b19
Ivy <gericaleksandra@gmail.com>
2017-08-28 19:07:28 +0200

    Final Grade - gets user input, uses three different methods

--- src/com/ivy/Main.java
+++ src/com/ivy/Main.java
@@ -3,6 +3,7 @@ package com.ivy;
 import java.util.*;
 
 public class Main {
+
     /**
      * This is Java's main method
      * <br>
@@ -12,23 +13,68 @@ public class Main {
      */
     public static void main(String[] args) {
         // write your code here
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your text:");
-        String input = scanner.nextLine().trim();
 
-        String trimmedInput = input.replaceAll("[^a-zA-Z0-9]", " ");
-        String noDoubleSpaceInput = trimmedInput.replaceAll("( )+", " ");
-        String[] words = noDoubleSpaceInput.split(" ");
-
-        Map<String, Integer> map = new HashMap<>();
-        for (String word : words) {
-            String key = String.format("%d letter words", word.length());
-            if (!map.containsKey(key)) {
-                map.put(key, 1);
-            } else {
-                map.put(key, map.get(key) + 1);
-            }
+        System.out.println("Please enter student's quiz score:");
+        int quiz = scanner.nextInt();
+        if (!validateInput(quiz)) {
+            System.out.println("Please insert valid score, 0 - 100 points");
+            return;
         }
-        System.out.println(Arrays.toString(map.entrySet().toArray()));
+        if (quiz < 50) {
+            System.out.println("Student failed the exam");
+            return;
+        }
+
+        System.out.println("Please enter student's midterm score:");
+        int midterm = scanner.nextInt();
+        if (!validateInput(midterm)) {
+            System.out.println("Please insert valid score, 0 - 100 points");
+            return;
+        }
+        if (midterm < 50) {
+            System.out.println("Student failed the exam");
+            return;
+        }
+
+        System.out.println("Please enter student's second semester score:");
+        int secondSemester = scanner.nextInt();
+        if (!validateInput(secondSemester)) {
+            System.out.println("Please insert valid score, 0 - 100 points");
+            return;
+        }
+        if (secondSemester < 50) {
+            System.out.println("Student failed the exam");
+            return;
+        }
+
+        int finalScore = calculateScore(quiz, midterm, secondSemester);
+        String grade = getGrade(finalScore);
+        System.out.println("Student's final score is: " + grade);
+    }
+
+    private static boolean validateInput(int score) {
+        return !(score < 0 && score > 100);
+    }
+
+    private static int calculateScore(int quiz, int midterm, int secondSemester) {
+        return (quiz + midterm + secondSemester) / 3;
+    }
+
+    private static String getGrade(int finalScore) {
+        String grade = "F";
+        if (finalScore > 90) {
+            grade = "A";
+        } else if (finalScore > 80) {
+            grade = "B";
+        } else if (finalScore > 70) {
+            grade = "C";
+        } else if (finalScore > 60) {
+            grade = "D";
+        } else if (finalScore > 50) {
+            grade = "E";
+        }
+        return grade;
     }
 }
