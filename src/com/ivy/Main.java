9cdb2e9369eca62b455de8f3cc678f60231f9d69
Ivy <gericaleksandra@gmail.com>
2017-10-16 12:14:11 +0200

    Vending Machine

--- src/com/ivy/Main.java
+++ src/com/ivy/Main.java
@@ -1,37 +1,57 @@
 package com.ivy;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
-    /**
-     * This is Java's main method
-     * <br>
-     * It always looks like this and each program must have one!
-     *
-     * @param args the arguments passed to the program
-     */
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.##");
+
     public static void main(String[] args) {
-
-        System.out.println("Enter your time table number: ");
-
         Scanner input = new Scanner(System.in);
-        int order = input.nextInt();
 
-        System.out.println("Here is the timetable for number " + order);
-        System.out.println();
+        System.out.println("Product price: ");
+        double price = input.nextDouble();
 
-        for (int row = 1; row <= order; row++) {
-            for (int column = 1; column <= order; column++) {
-                System.out.print(getProduct(row, column) + " ");
-            }
-            System.out.println();
-        }
+        System.out.println("Insert cash: ");
+        double cash = input.nextDouble();
+
+        cashReturn(cash, price);
     }
 
-    private static Object getProduct(int firstOperand, int secondOperand) {
-        return firstOperand * secondOperand;
+    static int[] cashReturn(double insertedCash, double priceOfItem) {
+        int[] changeToReturn = new int[]{-1, -1, -1, -1, -1};
 
+        if (priceOfItem < 0.1) {
+            System.out.println("Please insert valid price.");
+            return changeToReturn;
+        }
+
+        if (insertedCash < priceOfItem) {
+            System.out.println("Insufficient funds.");
+            return changeToReturn;
+        }
+
+        double fullChange = insertedCash - priceOfItem;
+        System.out.println("Your fullChange is " + Double.parseDouble(DECIMAL_FORMAT.format(fullChange)));
+
+        int dollars = (int) Math.floor(fullChange);
+        int decimalChange = (int) (fullChange * 100 - dollars * 100);
+        int quarters = decimalChange / 25;
+        decimalChange = decimalChange % 25;
+        int dimes = (decimalChange / 10);
+        decimalChange = decimalChange % 10;
+        int nickles = (decimalChange / 5);
+        int cents = decimalChange % 5;
+
+        System.out.println("Cashback: " + dollars + " dollars " + quarters + " quarters " + dimes + " dimes " + nickles + " nickles " + cents + " cents");
+
+        changeToReturn[0] = dollars;
+        changeToReturn[1] = quarters;
+        changeToReturn[2] = dimes;
+        changeToReturn[3] = nickles;
+        changeToReturn[4] = cents;
+
+        return changeToReturn;
     }
 }
-
